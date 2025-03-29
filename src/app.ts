2c73f91fa488410c14f050906bf9fdab78f74c86
marco-oliveira7 <ma8155354@gmail.com>
2025-03-29 12:09:54 -0300

    cors implemented

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { json } from "express";
 import router from "./routes";
+import cors from "cors"
 
 function createApp() {
   const app = express();
 
+  const corsOptions = {
+    methods: ["GET", "POST", "DELETE", "PATCH",]
+  }
+
   app.use(json());
-
   app.use("/api", router);
-
+  app.use(cors(corsOptions))
   return app
 }
 
